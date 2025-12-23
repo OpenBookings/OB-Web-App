ec70947e097191cff36d957c7c0a99299f1f96a9
Wouter van der Wal <81486986+wouter1203@users.noreply.github.com>
2025-12-23 12:16:12 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from "next";
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data: blob:;
+  font-src 'self';
+  connect-src 'self' https:;
+  frame-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+`.replace(/\s{2,}/g, " ").trim();
+
 const nextConfig: NextConfig = {
   async headers() {
     return [
@@ -9,6 +22,10 @@ const nextConfig: NextConfig = {
           {
             key: "Strict-Transport-Security",
             value: "max-age=86400",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy,
           },
         ],
       },
